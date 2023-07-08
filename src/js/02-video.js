b924cd621a815126331f8588cd6840bd1d993164
MakEva <sher.evikm@gmail.com>
2023-07-08 19:33:42 +0300

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iFrame = document.querySelector("iframe");
 const player = new Player(iFrame);
@@ -6,25 +7,26 @@ const player = new Player(iFrame);
 const LS = "videoplayer-current-time";
 const currentTime = function (evt) {
     localStorage.setItem(LS, evt.seconds);
+    console.log(evt.seconds);
 } 
-console.log(currentTime);
 
-player.on("timeupdate", currentTime)
+player.on("timeupdate", throttle(currentTime, 1000));
 // console.log(player);
 
+const lastTime = Number(localStorage.getItem(LS));
 
-// player.setCurrentTime(30.456).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
+player.setCurrentTime(lastTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
 
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
+        default:
+            // some other error occurred
+            break;
+    }
+});
 
 
